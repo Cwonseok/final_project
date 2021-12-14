b3f894585dea133d4af3575ca303edd8a3936a67
Cwonseok <85975787+Cwonseok@users.noreply.github.com>
2021-12-15 00:29:08 +0900

    first commit

---
+++ po/src/main/java/cat/po/PoApplication.java
@@ -0,0 +1,13 @@
+package cat.po;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoApplication.class, args);
+	}
+
+}
